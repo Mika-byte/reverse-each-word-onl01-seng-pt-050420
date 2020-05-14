83921479d835135b10be8cdea521b0f153407b0b
Michaelle Barbor <michaelle.barbor@outlook.com>
2020-05-14 17:18:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,3 @@
-def reverse_each_word(words)
-  new_array = []
-  words.collect do |word|
-  new_array << word.reverse
-  end
-  return new_array
-end
-
 
 def reverse_word(sentence1)
   sentence2 = sentence1.split(//)
