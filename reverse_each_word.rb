b3ad83acc54695c183a253a50774d140cc8853d2
Michaelle Barbor <michaelle.barbor@outlook.com>
2020-05-14 16:24:10 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(words)
-new_array = []
-words.collect do |word|
-new_array << word
-end
-return new_array
+  new_array = []
+  words.collect do |word|
+  new_array << word
+  end
+  return new_array
 end
